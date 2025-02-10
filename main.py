e66a7773b82200143488ef8099b4fc0817dc3158
Renuprasad Gutha <42505386+renuprasadg@users.noreply.github.com>
2025-02-10 10:15:49 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import pickel
+from fastapi import FastAPI
+from pydantic import BaseModel
+with open("iris_model.pkl",'rb') as f:
+  model = pickel.load(f)
+app = FastAPI()
+class IrisInput(BaseModel):
+  sl:float
+  sw:float
+  pl:float
+  pw:float
+@app.get("/")
+def read_root():
+  return{"message":"welcome to fastapi"}
+@app.post("/predict/")
+def predict(data:IrisInput):
+  input_data = [[data.sl,data.sw,data.pl,data.pw]]
+  prediction = model.predict(input_data)[0]
+  return {'prediction is:',int(prediction)}
+  
